de11332df77c3b83b07200ec8550301f59918295
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-10-03 00:22:51 -0400

    add projects button w click functionality

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// load projects on button click
+var project_btn = document.querySelector(".project-button");
+
+project_btn.addEventListener("click", function() {
+  document.location =  "/projects.html";
+});
